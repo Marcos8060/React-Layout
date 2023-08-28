36e284c36ce6051dc459f42d4aa36f24aa28aec5
MarcosDMT <marcosgav80@gmail.com>
2023-08-28 16:42:17 +0300

    initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='navigation'>
+    <p>We are the Navigation</p>
+    <p>We are the Navigation</p>
+    <p>We are the Navigation</p>
+    <p>We are the Navigation</p>
+    </div>
+  )
+}
+
+export default Navigation
